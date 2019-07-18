a647f5315b0c52c853934f4df4d16548537d76b1
lyubchyk09 <lyubchyk29@gmail.com>
2019-07-18 15:32:15 +0300

    I create class VolumeBaseSquare

---
+++ src/volume/VolumeBaseSquare.java
@@ -0,0 +1,28 @@
+package volume;
+
+import shapes.Square;
+
+/**
+ * Created by Lyubchyk Manko
+ */
+public class VolumeBaseSquare extends Square  implements Volume{
+
+    private double height;
+
+    public VolumeBaseSquare(double side, double height){
+        super(side);
+        this.height = height;
+    }
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    @Override
+    public double getVolume() {
+        return height * getArea();
+    }
+}
