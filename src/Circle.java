b0d4d53d858d7aa891bab82a15fde9814628b951
111 <azdronik@gmail.com>
2019-07-18 12:46:47 +0300

    Added class Circle

---
+++ src/Circle.java
@@ -0,0 +1,12 @@
+public class Circle extends ShapesWithoutAngles {
+
+
+    public Circle(double radius) {
+        super(radius);
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * getRadius() * getRadius();
+    }
+}
