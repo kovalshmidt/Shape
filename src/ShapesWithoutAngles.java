200b0ed758bf66cfe3e56e1b83655977187d85b9
Lenovo <petrokoltutsku@mail.com>
2019-07-18 12:32:06 +0300

    Created class ShapesWithoutAngles

---
+++ src/ShapesWithoutAngles.java
@@ -0,0 +1,20 @@
+public abstract class ShapesWithoutAngles implements Shape {
+
+    private double radius;
+
+    public ShapesWithoutAngles(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public abstract double getArea();
+
+}
