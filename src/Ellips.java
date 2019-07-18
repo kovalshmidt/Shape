a32f2bd1ad4904b07bf975d4f57cd0fd528cb3db
userPC <viktoria.grenyuh@gmail.com>
2019-07-18 13:04:21 +0300

    Added Vika We created new class Elips to find area.

--- src/Ellips.java
+++ src/Ellips.java
@@ -1,11 +1,22 @@
 public abstract class Ellips extends ShapesWithoutAngles {
 
+    private double radiusTwo;
+
+
+    public double getRadiusTwo() {
+        return radiusTwo;
+    }
+
+    public void setRadiusTwo(double radiusTwo) {
+        this.radiusTwo = radiusTwo;
+    }
+
     public Ellips(double radiusOne) {
         super(radiusOne);
     }
 
 
-    public double getArea(double radiusTwo){
+    public double getArea(){
         return getRadius() * radiusTwo * Math.PI;
     }
 }
